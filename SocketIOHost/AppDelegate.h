5f56521b2ead1a7c9d316101b85d0577de82e7bb
Patrick Perini <pcperini@gmail.com>
2014-09-03 15:42:40 -0400

    add underlying App to enable testing

---
+++ SocketIOHost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocketIOHost
+//
+//  Created by Patrick Perini on 9/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocketIOHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketIOHost
+//
+//  Created by Patrick Perini on 9/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
