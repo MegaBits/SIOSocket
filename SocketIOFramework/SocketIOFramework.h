24e973f9509c47cd16e2d59f28c57bdabd12d912
vasarhelyia <vasarhelyi.agi@gmail.com>
2014-12-30 12:33:10 +0100

    Added framework target.

---
+++ SocketIOFramework/SocketIOFramework.h
@@ -0,0 +1,18 @@
+//
+//  SocketIOFramework.h
+//  SocketIOFramework
+//
+//  Created by Agnes Vasarhelyi on 30/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocketIOFramework.
+FOUNDATION_EXPORT double SocketIOFrameworkVersionNumber;
+
+//! Project version string for SocketIOFramework.
+FOUNDATION_EXPORT const unsigned char SocketIOFrameworkVersionString[];
+
+#import <SocketIOFramework/SIOSocket.h>
+#import <SocketIOFramework/socket.io.js.h>
